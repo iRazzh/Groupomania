24fd1c206bbe3923ce053a26a9fd13cbbcdf71fe
iRazzh <tanguygerardin60@hotmail.com>
2021-05-12 16:06:44 +0200

    add multer

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -4,15 +4,18 @@ const router = express.Router();
 // Important de "auth" qui permet de sécuriser les routes
 const auth = require('../middleware/auth');
 
+// Important de "multer" pour gérer les images
+const multer = require('../middleware/multer-config');
+
 // Importation du "controller" concernant les "post"
 const postCtrl = require('../controllers/post');
 
 // Rajouter le multer (a voir sur Git si jamais)
 
 // Router de la création du post
-router.post('/', auth, postCtrl.createPost);
+router.post('/', auth, multer, postCtrl.createPost);
 // Router de la modification du post
-router.put('/:id', auth, postCtrl.modifyPost);
+router.put('/:id', auth, multer, postCtrl.modifyPost);
 // Router de la suppression du post
 router.delete('/:id', auth, postCtrl.deletePost);
 // Router de l'accès à tous les posts 
